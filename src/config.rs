f35e7b69831a2948ff2edc967997d3ac580972cf
dearfl <i@flr.me>
2025-01-09 21:57:24 +0800

    add tests & comment for config

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,17 @@
 /// Config is the common parameters for steam api requests.
-#[derive(Debug, Default, Clone)]
+/// Most of steam http api have 3 common parameters:
+///   - key: String, the API KEY used for requesting these APIs.
+///   - format: json/xml/vdf, format specified for response, default value: json.
+///   - language: language specified for response, by default all response is in english.
+/// # Example
+/// ```
+/// use kez::Config;
+/// let config = Config::from("MY_STEAM_API_KEY");
+/// ```
+#[derive(Debug, Clone)]
 pub struct Config {
     key: String,
-    // TODO:  add optional parameter format & language
+    // TODO:  add optional parameter: format & language
 }
 
 impl<S> From<S> for Config
