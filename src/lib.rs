9e6ffdb4352a0aeb16e6c2d88ecc8226003648c6
dearfl <i@flr.me>
2025-01-09 23:52:19 +0800

    add trait TransformRequest

--- src/lib.rs
+++ src/lib.rs
@@ -6,4 +6,13 @@ pub use client::Client;
 pub use config::Config;
 pub use error::Error;
 
+use reqwest::RequestBuilder;
+
 pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+/// TransformRequest is trait for literally transform request like add parameters.
+/// Implement this trait for Config and varies Parameters make the inner get method
+/// Generic.
+pub trait TransformRequest {
+    fn transform_request(&self, req: RequestBuilder) -> RequestBuilder;
+}
