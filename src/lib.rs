2ebc9e36fc212650e2e679c46bbbb126569ee42f
dearfl <i@flr.me>
2025-01-10 18:17:57 +0800

    hide trait TransformRequest and impl it for Option<T>

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,16 @@ pub type Result<T, E = Error> = std::result::Result<T, E>;
 /// TransformRequest is trait for literally transform request like add parameters.
 /// Implement this trait for Config and varies Parameters make the inner get method
 /// Generic.
-pub trait TransformRequest {
+trait TransformRequest {
     fn transform_request(&self, req: RequestBuilder) -> RequestBuilder;
+}
+
+/// impl TransformRequest for Option<T> where T: Transform for ease of use
+impl<T: TransformRequest> TransformRequest for Option<T> {
+    fn transform_request(&self, req: RequestBuilder) -> RequestBuilder {
+        match self {
+            Some(value) => value.transform_request(req),
+            None => req,
+        }
+    }
 }
