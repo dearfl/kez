a48e098d560b494a465c4779debd4d9f8d9c92da
dearfl <i@flr.me>
2025-01-10 12:37:50 +0800

    add an example for daily api update check

---
+++ examples/detect-valve-api-change.rs
@@ -0,0 +1,42 @@
+/// This is an simple example used to check Valve API updates.
+/// The example will return a failure if any API is updated.
+use clap::Parser;
+use kez::{
+    dota2::{
+        get_match_history::MatchHistoryParameter,
+        get_match_history_by_seq_num::MatchHistoryBySeqNumParameter,
+    },
+    Client, Error,
+};
+
+#[derive(Parser)]
+pub struct Args {
+    pub key: String,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    let client = Client::new(args.key)?;
+    let result = client
+        .get_match_history(MatchHistoryParameter::default())
+        .await;
+    let para = match result {
+        Ok(history) => {
+            let match_seq_num = history
+                .matches
+                .iter()
+                .fold(0, |init, mat| std::cmp::max(init, mat.match_seq_num));
+            MatchHistoryBySeqNumParameter::start_at(match_seq_num)
+        }
+        Err(Error::DecodeError(err, content)) => {
+            anyhow::bail!("DecodeError: {err}\nContent: {content}");
+        }
+        Err(_) => MatchHistoryBySeqNumParameter::default(),
+    };
+    let result = client.get_match_history_by_seq_num(para).await;
+    if let Err(Error::DecodeError(err, content)) = result {
+        anyhow::bail!("DecodeError: {err}\nContent: {content}");
+    };
+    Ok(())
+}
