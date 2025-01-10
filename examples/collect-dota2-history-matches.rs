00452630988e015ced12aeb7ac7a5b0b73a60779
dearfl <i@flr.me>
2025-01-10 11:34:32 +0800

    fill some detail for example

--- examples/collect-dota2-history-matches.rs
+++ examples/collect-dota2-history-matches.rs
@@ -1,6 +1,11 @@
+use std::time::Duration;
+
 use clap::Parser;
 use kez::Client;
 
+// By default this example tries to collect history matches with match_seq_num between 0-1000
+// with a request interval of 10s
+// An API KEY is required to use STEAM WEB API. You can accquire one from https://steamcommunity.com/dev/apikey
 #[derive(Parser)]
 pub struct Args {
     #[arg(long, default_value_t = 10000)]
@@ -22,14 +27,20 @@ async fn main() -> anyhow::Result<()> {
     let mut start = args.start;
     let end = args.end;
     while start < end {
-        println!("collecting {}...", start);
+        println!("collecting matches starting from {}", start);
         let result = client.get_match_history_by_seq_num((start, 100)).await?;
+
+        // update start match_seq_num
         start = result.matches.iter().fold(start + 1, |init, mat| {
-            println!("match_id: {}", mat.match_id);
+            println!("match: {:?}", mat);
             std::cmp::max(init, mat.match_seq_num + 1)
         });
-        // do something useful with result
+
+        // perhaps do something useful with result
+        // like save it to a database for later query.
         matches.extend(result.matches);
+
+        tokio::time::sleep(Duration::from_millis(args.interval)).await;
     }
     Ok(())
 }
