1690cd2d1a9b44ed08502df81e219368dcc49e33
dearfl <i@flr.me>
2025-01-10 17:59:48 +0800

    refine skill enum

--- src/dota2/skill.rs
+++ src/dota2/skill.rs
@@ -1,13 +1,18 @@
 /// Skill level of player or match.
 #[non_exhaustive]
 #[repr(u8)]
-#[derive(Clone, Copy, Debug, Default)]
+#[derive(Clone, Copy, Debug)]
 pub enum Skill {
-    #[default]
-    Any = 0,
+    Unknown(u8) = 0,
     Normal = 1,
     High = 2,
     VeryHigh = 3,
+}
+
+impl Default for Skill {
+    fn default() -> Self {
+        Self::Unknown(0)
+    }
 }
 
 impl From<u8> for Skill {
@@ -16,13 +21,18 @@ impl From<u8> for Skill {
             1 => Self::Normal,
             2 => Self::High,
             3 => Self::VeryHigh,
-            _ => Self::Any,
+            value => Self::Unknown(value),
         }
     }
 }
 
 impl From<Skill> for u8 {
     fn from(value: Skill) -> Self {
-        value as Self
+        match value {
+            Skill::Unknown(value) => value,
+            Skill::Normal => 1,
+            Skill::High => 2,
+            Skill::VeryHigh => 3,
+        }
     }
 }
